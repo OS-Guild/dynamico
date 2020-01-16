d91add9ffbcd1534f6aa0f523109aebaacafef93
Natalya Shrits <nataly@soluto.com>
2020-01-16 14:18:45 -0600

    parallel building

--- client/cli/src/actions/build.ts
+++ client/cli/src/actions/build.ts
@@ -1,4 +1,4 @@
-import { STRING } from 'caporal';
+import { BOOLEAN, STRING } from 'caporal';
 import { getComponentDirectories, getPackageJson } from '../../lib/utils';
 import { registerCommand } from '../util';
 import { build, BuildMode, BuildOptions } from '../../lib';
@@ -8,15 +8,27 @@ export default (config: DcmConfig) =>
   registerCommand({
     name: 'build',
     description: 'Build dynamic component',
-    options: [['-m, --mode <mode>', 'mode', STRING, BuildMode.development], ['-d, --dir <directory>', 'dir', STRING]],
-    action: async ({ options: { mode, dir }, logger }) => {
+    options: [
+      ['-m, --mode <mode>', 'mode', STRING, BuildMode.development],
+      ['-d, --dir <directory>', 'dir', STRING],
+      ['-p, --parallel', 'parallel', BOOLEAN, false]
+    ],
+    action: async ({ options: { mode, dir, parallel }, logger }) => {
       const dirs = getComponentDirectories(dir, config && config.workspaces);
 
-      for (const dir of dirs) {
+      const buildDir = async dir => {
         const { name } = getPackageJson(dir);
         logger.info(`Building ${name}...`);
         const buildConfig: BuildOptions = { mode, dir, modifyRollupConfig: config && config.modifyRollupConfig };
         await build(buildConfig);
+      };
+
+      if (parallel) {
+        return Promise.all(dirs.map(buildDir));
+      }
+
+      for (const dir of dirs) {
+        await buildDir(dir);
       }
     }
   });
